c7137b1f4d3be1a1d70076acd1aae43e2971da9a
nghiemtuan <nghiem.tien.tuan@sun-asterisk.com>
2020-11-02 15:40:13 +0700

    base oauth api

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,12 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// OAuth
+Route::group(['namespace' => 'api'], function () {
+    Route::post('login', 'UserController@login');
+    Route::post('register', 'UserController@register');
+
+    Route::group(['middleware' => 'auth:api'], function() {
+        Route::get('details', 'UserController@details');
+    });
 });
